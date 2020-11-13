4bfc0f497421f85eb273eb423f7f512b64cf3f77
toenteen <71368747+toenteen@users.noreply.github.com>
2020-11-13 15:29:44 +0700

    Update show.php

--- show.php
+++ show.php
@@ -23,7 +23,7 @@ $res = mysqli_query($conn, 'SELECT * FROM guestbook');
 while($Result = mysqli_fetch_array($res))
 {
 ?>
-  <tr>
+  <tr class="table-info">
     <td><?php echo $Result['Name'];?></div></td>
     <td><?php echo $Result['Comment'];?></td>
     <td><?php echo $Result['Link'];?></td>
