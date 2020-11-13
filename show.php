546e4f1dd7ad2ca0009ddef16725a1c78ac7fa4e
toenteen <71368747+toenteen@users.noreply.github.com>
2020-11-13 16:46:52 +0700

    Update show.php

--- show.php
+++ show.php
@@ -30,6 +30,7 @@ while($Result = mysqli_fetch_array($res))
     <td><?php echo $Result['Comment'];?></td>
     <td><?php echo $Result['Link'];?></td>
     <td><button type="button" class="btn btn-info">Add</button>    <button type="button" class="btn btn-info">Remove</button></td>
+    <td><button type="button" class="btn btn-info">Add</button></td>
   </tr>
 <?php
 }
