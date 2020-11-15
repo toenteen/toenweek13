f07f176fc2bbe72af81d1049d16cda88369836fe
toenteen <71368747+toenteen@users.noreply.github.com>
2020-11-15 19:47:36 +0700

    Add files via upload

---
+++ del.php
@@ -0,0 +1,16 @@
+<?php
+$id=$_GET['ID'];
+$conn = mysqli_init();
+mysqli_real_connect($conn, 'toenweek13.mysql.database.azure.com', 'toenteen@toenweek13', 'Kassinowa751', 'itflab', 3306);
+$sql="DELETE FROM guestbook WHERE ID='$id'";
+if (mysqli_connect_errno($conn))
+{
+    die('Failed to connect to MySQL: '.mysqli_connect_error());
+}
+if (mysqli_query($conn, $sql)) {
+    header("Location: index.php");
+  } else {
+    echo "Error: " . $sql . "<br>" . mysqli_error($conn);
+  }
+mysqli_close($conn);
+?>
