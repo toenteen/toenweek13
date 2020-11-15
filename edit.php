b4d8b8e2c69c7ff9de0f440ec1196ad593907616
toenteen <71368747+toenteen@users.noreply.github.com>
2020-11-15 20:17:05 +0700

    Update edit.php

--- edit.php
+++ edit.php
@@ -6,8 +6,8 @@ mysqli_real_connect($conn, 'toenweek13.mysql.database.azure.com', 'toenteen@toen
 $res = mysqli_query($conn, "SELECT * FROM guestbook WHERE ID='$id'");
 $row = mysqli_fetch_array($res)
 ?>
-<form action = "update.php?ID=<?php echo $row['ID']; ?>" method = "post" id="CommentForm">
-    Name:<br><span class="badge badge-secondary">
+<span class="badge badge-secondary"><form action = "update.php?ID=<?php echo $row['ID']; ?>" method = "post" id="CommentForm">
+    Name:<br>
     <input type="text" name = "Name" id="idName" value="<?php echo "$row[Name]"; ?>" </span><br>
     Comment:<br>
     <input type="text" name = "Comment" id="idComment" value="<?php echo "$row[Comment]"; ?>" <br>
