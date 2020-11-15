e29c7a85a8edc058d1d020ecdb5b2f52e1e353b5
toenteen <71368747+toenteen@users.noreply.github.com>
2020-11-15 20:00:12 +0700

    Add files via upload

---
+++ update.php
@@ -0,0 +1,15 @@
+<?php
+$id=$_GET['ID'];
+$conn = mysqli_init();
+mysqli_real_connect($conn, 'toenweek13.mysql.database.azure.com', 'toenteen@toenweek13', 'Kassinowa751', 'itflab', 3306);
+$name=$_POST['Name'];
+$comment=$_POST['Comment'];
+$link=$_POST['Link'];
+$sql="UPDATE guestbook SET Name='$name',Comment='$comment',Link='$link' WHERE ID='$id'";
+if (mysqli_query($conn, $sql)) {
+    header("Location: index.php");
+  } else {
+    echo "Error: " . $sql . "<br>" . mysqli_error($conn);
+  }
+mysqli_close($conn);
+?>
