39878a54c40055046d527944453b7df01ecd9696
toenteen <71368747+toenteen@users.noreply.github.com>
2020-11-16 21:14:43 +0700

    Update show.php

--- show.php
+++ show.php
@@ -30,26 +30,27 @@ $res = mysqli_query($conn, 'SELECT * FROM guestbook');
         <td><?php echo $Result['Name'];?></div></td>
         <td><?php echo $Result['Comment'];?></td>
         <td><?php echo $Result['Link'];?></td>
-        <td><a href="edit.php?ID=<?php echo $Result['ID']?><button type="button" class="btn btn-info">Edit</button></a>    <button type="button" class="btn btn-danger" data-toggle="modal" data-target="#staticBackdrop">Remove</button></td>
-        <div class="modal fade" id="staticBackdrop" data-backdrop="static" tabindex="-1" role="dialog" aria-labelledby="staticBackdropLabel" aria-hidden="true">
-            <div class="modal-dialog" role="document">
-             <div class="modal-content">
-                 <div class="modal-header">
-                <h5 class="modal-title" id="staticBackdropLabel">Modal title</h5>
-                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-            <span aria-hidden="true">&times;</span>
-          </button>
+        <td><a href="edit.php?ID=<?php echo $Result['ID']?><button type="button" class="btn btn-info">Edit</button></a>    <button type="button" class="btn btn-danger" data-toggle="modal" data-target="#staticBackdrop">Remove</button>
+            <div class="modal fade" id="staticBackdrop" data-backdrop="static" tabindex="-1" role="dialog" aria-labelledby="staticBackdropLabel" aria-hidden="true">
+                <div class="modal-dialog" role="document">
+                    <div class="modal-content">
+                        <div class="modal-header">
+                            <h5 class="modal-title" id="staticBackdropLabel">Modal title</h5>
+                            <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                        <span aria-hidden="true">&times;</span>
+                      </button>
+                        </div>
+                        <div class="modal-body">
+                            ...
+                        </div>
+                        <div class="modal-footer">
+                            <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+                            <a href="del.php?ID=<?php echo $Result['ID']?>" class="btn btn-danger" onclick="return confirm('Are you sure you want to delete this item?')">Remove</a>
+                        </div>
+                    </div>
+                </div>
             </div>
-            <div class="modal-body">
-                ...
-            </div>
-            <div class="modal-footer">
-                <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
-                <a href="del.php?ID=<?php echo $Result['ID']?>" class="btn btn-danger" onclick="return confirm('Are you sure you want to delete this item?')">Remove</a>
-            </div>
-        </div>
-    </div>
-</div>
+            </td>
       </tr>
     <?php
     }
