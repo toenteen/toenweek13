38e7b79eadb5b2c5ed169b021b658f517c2683f4
toenteen <71368747+toenteen@users.noreply.github.com>
2020-11-16 20:31:35 +0700

    Update show.php

--- show.php
+++ show.php
@@ -13,28 +13,30 @@ if (mysqli_connect_errno($conn))
 }
 $res = mysqli_query($conn, 'SELECT * FROM guestbook');
 ?>
-<table width="600" border="1" class="table table-dark table-striped table-hover">
-  <tr>
-    <th width="100"> <div align="center">Name</div></th>
-    <th width="350"> <div align="center">Comment </div></th>
-    <th width="150"> <div align="center">Link </div></th>
-    <th width="150"> <div align="center">Action </div></th>
-    
-  </tr>
-<?php
-while($Result = mysqli_fetch_array($res))
-{
-?>
-  <tr class="table-info">
-    <td><?php echo $Result['Name'];?></div></td>
-    <td><?php echo $Result['Comment'];?></td>
-    <td><?php echo $Result['Link'];?></td>
-    <td><a href="edit.php?ID=<?php echo $Result['ID']?><button type="button" class="btn btn-info">Edit</button></a>    <a href="del.php?ID=<?php echo $Result['ID']?>" class="btn btn-danger"onclick="return confirm('Confirm data deletion?')">Remove</a></td>
-  </tr>
-<?php
-}
-?>
-</table>
+<div class="table-responsive">
+    <table width="600" border="1" class="table table-dark table-striped table-hover">
+      <tr>
+        <th width="100"> <div align="center">Name</div></th>
+        <th width="350"> <div align="center">Comment </div></th>
+        <th width="150"> <div align="center">Link </div></th>
+        <th width="150"> <div align="center">Action </div></th>
+
+      </tr>
+    <?php
+    while($Result = mysqli_fetch_array($res))
+    {
+    ?>
+      <tr class="table-info">
+        <td><?php echo $Result['Name'];?></div></td>
+        <td><?php echo $Result['Comment'];?></td>
+        <td><?php echo $Result['Link'];?></td>
+        <td><a href="edit.php?ID=<?php echo $Result['ID']?><button type="button" class="btn btn-info">Edit</button></a>    <a href="del.php?ID=<?php echo $Result['ID']?>" class="btn btn-danger"onclick="return confirm('Confirm data deletion?')">Remove</a></td>
+      </tr>
+    <?php
+    }
+    ?>
+    </table>
+</div>
 <td><a href="https://toenweek13.azurewebsites.net/form.html"><button type="button" class="btn btn-success">Add</button></a></td>
 <?php
 mysqli_close($conn);
